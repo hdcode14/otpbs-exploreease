6fcb09e8abc24a72d8cd57daeb97b0805a8d4564
hdcode14 <hrisihd18@gmail.com>
2025-11-16 15:02:27 +0530

    configuration in config.py

--- config.py
+++ config.py
@@ -4,9 +4,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'exploreease-secret-key-2025'
     ADMIN_SECRET_KEY = os.environ.get('ADMIN_SECRET_KEY') or 'admin123'
     UPLOAD_FOLDER = 'static/uploads'
-    
-    # For production - will be set by Render
-    if 'DATABASE_URL' in os.environ:
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
-    else:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
